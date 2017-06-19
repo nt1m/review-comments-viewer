ed0c92b85244ddb2a9dc044a3d079ec6d6460dba
Tim Nguyen <ntim.bugs@gmail.com>
2017-06-19 14:31:06 +0200

    Add nice buttons

--- components/comment.js
+++ components/comment.js
@@ -4,32 +4,36 @@ function Comment({ comment }) {
   let isOpened = !!stateManager.getState().opened.find(c => c.id == comment.id);
   return createElement("div", {
     className: "comment",
-    onClick() {
-      if (port && isOpened) {
-        port.sendMessage({ file: comment.file, lineNumber: comment.lineNumber });
-      }
-    }
-
   },
     createElement("code", {
       className: "comment-diff",
     }, comment.diff),
-    createElement("p", {
-      className: "comment-body",
-    }, comment.body),
-    createElement("button", {
-      className: isOpened ? "comment-resolve" : "comment-reopen",
-      onClick(e) {
-        const state = stateManager.getState();
-        if (isOpened) {
-          stateManager.setState({ opened: state.opened.filter(c => c.id !== comment.id) });
-          stateManager.state.resolved.push(comment);
-        } else {
-          stateManager.setState({ resolved: state.resolved.filter(c => c.id !== comment.id) });
-          stateManager.state.opened.push(comment);
+    createElement("div", {
+      className: "comment-contents",
+    },
+      createElement("p", {
+        className: "comment-body",
+      }, comment.body),
+      (typeof port !== "undefined" && isOpened) && createElement("button", {
+        className: "comment-view-file",
+        onClick() {
+          port.sendMessage({ file: comment.file, lineNumber: comment.lineNumber });
         }
-        e.stopPropagation();
-      }
-    }, isOpened ? "Resolve" : "Reopen")
+      }, "View file"),
+      createElement("button", {
+        className: isOpened ? "comment-resolve" : "comment-reopen",
+        onClick(e) {
+          const state = stateManager.getState();
+          if (isOpened) {
+            stateManager.setState({ opened: state.opened.filter(c => c.id !== comment.id) });
+            stateManager.state.resolved.push(comment);
+          } else {
+            stateManager.setState({ resolved: state.resolved.filter(c => c.id !== comment.id) });
+            stateManager.state.opened.push(comment);
+          }
+          e.stopPropagation();
+        }
+      }, isOpened ? "Resolve" : "Reopen")
+    )
   );
 }
